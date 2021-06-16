a99d1abfdda5e217e38630cd205a5076a58e6a54
Naseem Oyebola <naseemoyebola@gmail.com>
2021-06-16 22:28:42 +0100

    feat: add item

--- ClassicTodo/ClassicTodo/TodoViewController.swift
+++ ClassicTodo/ClassicTodo/TodoViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class TodoViewController: UITableViewController {
 
-    let itemArray = [ "Find Mike", "Buy Eggs", "Play football"]
+    var itemArray = [ "Find Mike", "Buy Eggs", "Play football"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,5 +41,23 @@ class TodoViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // what will happen once the user clicks the add bar button
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+           textField = alertTextField
+            
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
